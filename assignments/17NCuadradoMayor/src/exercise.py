2e956d73ad463fbd86b93667c148f565923de02c
RCERVANT3S <89056561+RCERVANT3S@users.noreply.github.com>
2021-09-07 11:12:12 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -4,5 +4,13 @@ def main():
     #Escribe tu código debajo de esta línea
     pass
 
+num = int(input("Escribe un numero : "))
+
+for  conta in range(1,num,1):
+ if  conta ** 2 >= num:
+    break  
+
+print(conta)
+
 if __name__=='__main__':
     main()
