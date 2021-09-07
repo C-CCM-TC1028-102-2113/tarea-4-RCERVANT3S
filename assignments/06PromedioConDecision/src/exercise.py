73bae0b1e86edeb74c24d2fb5b9acb76f853e989
RCERVANT3S <89056561+RCERVANT3S@users.noreply.github.com>
2021-09-07 11:10:41 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,23 @@
 def main():
-    #escribe tu código abajo de esta línea
+  #escribe tu código abajo de esta línea
     pass
-if __name__=='__main__':
+promedio = 0
+contador = 0
+
+a = float(input())
+if a >= 0:
+    promedio = promedio + a
+    contador = contador +  1
+
+while a >= 0: 
+    a = float(input())
+    if a >= 0:
+        promedio = promedio + a
+        contador = contador +  1
+    else: 
+        promedio1 = promedio / contador 
+
+print(promedio1)
+
+if __name__ == '__main__':
     main()
