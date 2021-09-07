4ddd19eeb9a99af29b9e29dfbc60cac1db59e777
RCERVANT3S <89056561+RCERVANT3S@users.noreply.github.com>
2021-09-07 11:12:59 -0500

    Update exercise.py

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,8 +1,32 @@
 
 def main():
-    #Escribe tu código debajo de esta línea
+    height = int(input("Enter triangle height: "))
+    #escribe tu código abajo de esta línea
     pass
 
+num = int(input("Enter triangle height: "))
+val = 2*num - 2
+ 
+for i in range(0, num):
+     
+       
+        for a in range(0, val):
+            print(end=" ")
+     
+        
+        val = val - 2
+     
+       
+        for a in range(0, i+1):
+         
+           
+            print("* ", end="")
+     
+       
+        print("\r")
 
-if __name__=='__main__':
+
+
+
+if __name__ == '__main__':
     main()
