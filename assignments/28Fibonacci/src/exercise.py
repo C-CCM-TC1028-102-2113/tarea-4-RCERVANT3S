f308fded4c0eefad72763984ab9b84b4a2488bed
RCERVANT3S <89056561+RCERVANT3S@users.noreply.github.com>
2021-09-07 21:09:27 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -3,5 +3,17 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+def fibo(num):
+    if num <2:
+        return num
+    else:
+        return  fibo(num-1) + fibo(num-2)
+
+
+num = int(input("Enter a number: "))
+
+res = fibo(num)
+print(res)
+
 if __name__=='__main__':
     main()
